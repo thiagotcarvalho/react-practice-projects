42ca6e0d51cbca701decf18fb796438afce75a47
Thiago Carvalho <tdecarv@gmail.com>
2024-02-29 21:30:03 -0500

    Add styling

--- src/app/accordian/page.jsx
+++ src/app/accordian/page.jsx
@@ -12,15 +12,15 @@ export default function Accordian() {
 
   console.log(selected);
 
-  return <div className='wrapper'>
-    <div className='accordian'>
+  return <div className='flex justify-center items-center h-screen w-screen'>
+    <div className='w-500px'>
       {
         data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div key={dataItem.id} className='item'>
+            <div key={dataItem.id} className='bg-teal-400 mb-10 px-20 py-10'>
               <div
                 onClick={() => handleSingleSelection(dataItem.id)}
-                className='title'
+                className='flex justify-between items-center cursor-pointer text-white '
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
